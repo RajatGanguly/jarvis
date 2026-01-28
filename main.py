36c031f2721d7c762006ac63f3613334223390a9
RajatGanguly <rajatganguly01@gmail.com>
2026-01-28 22:42:40 +0530

    Basic AI model on llama3.2:latest, gemma3:4b

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ image_64 = base64.b64encode(image_bytes).decode("utf-8")
 
 response = ollama.generate(model="gemma3:4b", images=[image_64], prompt="Explain the image", stream=True)
 
-# response = ollama.generate(model="gemma3:1b", images=[image_64, image_64], prompt="Explain the image", system="Explain in funny way", stream=True)
+response = ollama.generate(model="gemma3:1b", images=[image_64, image_64], prompt="Explain the image", system="Explain in funny way", stream=True)
 
 for res in response:
     print(res["response"], end="")
